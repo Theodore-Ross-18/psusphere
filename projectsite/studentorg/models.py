77a77bbdde615086b0616e9a610e1139d2e005a0
pavseh <pavsehfx@gmail.com>
2025-03-06 10:05:28 +0800

    refactor admin.py

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -38,7 +38,7 @@ class Student(BaseModel):
     program = models.CharField(max_length=25, blank=True, null=True)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
 
-    def __def__(self):
+    def __str__(self):
         return f"{self.lastname}, {self.firstname}"
     
 
